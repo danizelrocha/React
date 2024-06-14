695ff8790890f4b4875266fe82728b291f67cb75
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2024-06-14 10:06:27 -0300

    ok

--- FUNDAMENTOS/fundamentos/src/App.js
+++ FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,13 @@
-
+// components
+import FirstComponent from './components/FirstComponent';
+// styles / css
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Funadamentos React</h1>
+     <FirstComponent/>
     </div>
   );
 }
